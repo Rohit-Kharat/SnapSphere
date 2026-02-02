66ea1861ec867003fd6b07743629ef4118f6856b
Rohit Kharat <kharatrohit1130@gmail.com>
2026-02-02 19:26:29 +0530

    hhttps to https

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -28,7 +28,7 @@ const Signup = () => {
     e.preventDefault();
     try {
       setLoading(true);
-      const res = await axios.post('hhttps://snapsphere-jwj8.onrender.com/api/v1/user/register', input, {
+      const res = await axios.post('https://snapsphere-jwj8.onrender.com/api/v1/user/register', input, {
         headers: { 'Content-Type': 'application/json' },
         withCredentials: true
       });
