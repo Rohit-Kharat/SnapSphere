ce17ab8e1d81b33e501ea8f9d51b4f4801a1b606
Rohit-Kharat <Kharatrohit1130@gmail.com>
2026-02-02 07:37:52 +0530

    change to access cookie

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,8 +1,13 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
-  withCredentials: true, // IMPORTANT (so cookie token goes)
+  baseURL: import.meta.env.VITE_API_URL, // must be set in Vercel
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true, // keep true ONLY if you use cookie auth
 });
+console.log("API BASE URL:", import.meta.env.VITE_API_URL);
+
 
 export default api;
