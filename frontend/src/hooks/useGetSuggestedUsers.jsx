2df49b0c3270e25b4461b24df347559cbf2de26a
Rohit Kharat <kharatrohit1130@gmail.com>
2026-02-03 21:41:31 +0530

    Update API useGetSuggestedUsers.js

--- frontend/src/hooks/useGetSuggestedUsers.jsx
+++ frontend/src/hooks/useGetSuggestedUsers.jsx
@@ -9,7 +9,7 @@ const useGetSuggestedUsers = () => {
   const fetchSuggestedUsers = useCallback(async () => {
     try {
       const res = await axios.get(
-        "http://localhost:8000/api/v1/user/suggested",
+        "https://snapsphere-jwj8.onrender.com/api/v1/user/suggested",
         { withCredentials: true }
       );
 
